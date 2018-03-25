42f13bb9abb84b865f001204a3e5b58f7b61c999
sujayneglur123 <sujayneglur123@gmail.com>
2018-03-24 20:35:11 -0400

    adding files

---
+++ IdeaProjects/DoSDetection/src/dos/Details.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dos;
+
+/**
+ *
+ * @author seabirds
+ */
+public class Details 
+{
+    String features[]={"duration","protocol_type","service","flag","src_bytes","dst_bytes","class"};
+    
+}
---
+++ IdeaProjects/DoSDetection/src/dos/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dos;
+
+/**
+ *
+ * @author seabirds
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FirstFrame ff=new FirstFrame();
+        ff.setVisible(true);
+        ff.setResizable(false);
+        ff.setTitle("DoS Attack Detection");
+    }
+}
